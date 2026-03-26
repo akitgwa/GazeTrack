aa0409f56800fdac2670e103713d0c11d0fecaac
swagatikabhairab <swagatikabhairabsahu.4260@gmail.com>
2026-03-26 11:23:08 +0530

    New images added and tested

--- student_data.py
+++ student_data.py
@@ -4,5 +4,11 @@ student_data = {
     "Smruti": "003",
     "Pragati": "004",
     "Somya": "005",
-    "Simran": "006"
+    "Simran": "006",
+    "Ankita": "007",
+    "Archana": "008",
+    "Gungun": "009",
+    "Divyanshu": "010",
+    "Lisa": "011",
+    "Arpita": "012"
 }
