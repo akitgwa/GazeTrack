9a05c0a980795952beaf7bf5213974628d48723d
swagatikabhairab <swagatikabhairabsahu.4260@gmail.com>
2026-04-20 13:52:32 +0530

    Replaced real student data with dummy data

--- student_data.py
+++ student_data.py
@@ -1,14 +1,5 @@
 student_data = {
-    "Swagatika": "001",
-    "Pratiksha": "002",
-    "Smruti": "003",
-    "Pragati": "004",
-    "Somya": "005",
-    "Simran": "006",
-    "Ankita": "007",
-    "Archana": "008",
-    "Gungun": "009",
-    "Divyanshu": "010",
-    "Lisa": "011",
-    "Arpita": "012"
+    "Student1": 101,
+    "Student2": 102,
+    "Student3": 103
 }
